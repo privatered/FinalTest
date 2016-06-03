1162129f4fd4f09963a8c811fcd7f7262883a384
fkfk123 <lulupro104home01@gmail.com>
2016-06-03 21:51:52 +0800

    11155

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+        int aa;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
